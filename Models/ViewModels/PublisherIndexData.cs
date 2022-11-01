4f30e0de5d656fbf4eb6fd28fa8b14c33e4dd885
UEmanuelT <uifalean_e@yahoo.com>
2022-11-01 15:27:57 +0200

    Laborator 4 inceput

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿using Uifalean_Emanuel_Lab2.Models;
+namespace Uifalean_Emanuel_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
